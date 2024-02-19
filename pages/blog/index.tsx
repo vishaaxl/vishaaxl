4c5675f017506c36ca830c3e88bf197585e3fa7e
Vishal Shukla <vishaaxl@gmail.com>
2024-02-19 20:57:07 +0530

    Update index.tsx to test

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -24,6 +24,7 @@ const Blog: NextPage<Props> = ({ posts }) => {
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗿</text></svg>"
           />
         </Head>
+        <a href="https://virtueanalytics.com/shouldnt-be-indexed">Test</a>
         <div className="text-3xl">Latest Blogs</div>
         <div className="center flex-wrap flex-col">
           {posts.slice(0, 3).map((post: any, index: string) => (

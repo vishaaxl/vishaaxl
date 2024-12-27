7d805ee229330e93c31813f6ff6a27c5c66e2b70
Vishal Shukla <vishal.shukla@virtueanalytics.com>
2024-12-27 16:07:17 +0530

    fixed menu aligns

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -11,7 +11,7 @@ export default function Menu({}: MenuProps) {
   return (
     <div className="bg-white md:bg-transparent">
       <div className="container">
-        <ul className="[&>*]:pb-2 py-2 md:py-4 font-medium flex md:gap-8 gap-4 capitalize text-sm md:text-lg justify-end">
+        <ul className="py-4 md:py-4 font-medium flex md:gap-8 gap-4 capitalize text-base md:text-lg justify-end">
           <li>
             <Link href="/">Home</Link>
           </li>

a5e58749e5a1d31542b275a7cf3571c77924ed8e
vishaaxl <vishaaxl@gmail.vom>
2024-12-29 21:06:25 +0530

    added yml styles

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -5,6 +5,8 @@ import { getPostBySlug } from "@/utils/posts";
 
 import "prismjs/themes/prism-tomorrow.css";
 import "prismjs/components/prism-go.min.js";
+import "prismjs/components/prism-yaml.min.js";
+import "prismjs/components/prism-bash.min.js";
 
 interface PageProps {
   params: {
@@ -12,7 +14,7 @@ interface PageProps {
   };
 }
 
-export default async function Page({ params }: PageProps) {
+export default async function Blog({ params }: PageProps) {
   const { slug } = params;
 
   const post: any = getPostBySlug(slug);
@@ -26,7 +28,7 @@ export default async function Page({ params }: PageProps) {
   const contentHtml = md.render(post.content);
 
   return (
-    <div className="container pt-8">
+    <div className="container-blog pt-8">
       <div className="py-10">
         <h1 className="feature-title text-4xl lg:text-6xl font-semibold leading-tight">
           {post.title}
